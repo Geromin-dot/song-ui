77c2c56a8a427cc1406dd9be3d8ef42d00b7717a
Geromin-dot <grv_manese.student@ua.edu.ph>
2026-04-23 22:53:29 +0800

    feat: use environment variable for API base URL and update .gitignore

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "https://song-api-ljxm.onrender.com/manese/songs";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 
 export const getSongs = async () => {
   const response = await fetch(BASE_URL);
